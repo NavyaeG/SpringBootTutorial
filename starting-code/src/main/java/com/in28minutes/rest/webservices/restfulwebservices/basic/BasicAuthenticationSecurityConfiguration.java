7a17fe81df22d80de41cbd5a72203e586e77b060
NavyaeG <80058557+NavyaeG@users.noreply.github.com>
2024-04-29 01:36:16 +0530

    Add files via upload

---
+++ starting-code/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.in28minutes.rest.webservices.restfulwebservices.basic;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+//@Configuration
+public class BasicAuthenticationSecurityConfiguration {
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> auth.antMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest().authenticated());
+		//.antMatchers is used in basicAuthCheck function in HelloWorldController.java
+		http.httpBasic(Customizer.withDefaults());
+		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		http.csrf().disable();
+		return http.build();
+	}
+	
+}
---
+++ starting-code/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into todo (id,description,done,target_date,username) values 
+(10001,'Learn AWS',false,CURRENT_DATE(),'nav'),
+(10002,'Get AWS Certified',false,CURRENT_DATE(),'nav'),
+(10003,'Learn DevOps',false,CURRENT_DATE(),'nav'),
+(10004,'Go to the gym',false,CURRENT_DATE(),'nav');
